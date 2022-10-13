66478b5bd768aebe7a7e42c6d7ec3417f37d099a
JackArt237483 <vip.artem4567@mail.ru>
2022-10-13 13:25:00 +0300

    DZ1 lESSON 1

---
+++ Lesson-1/task 1/JS/app.js
@@ -0,0 +1,16 @@
+
+ const passportInput = document.querySelector(".passport-Input");
+ const passportCheck = document.querySelector(".passport-Check");
+ const passportResult = document.querySelector(".passport-Result");
+ 
+ const passportRegExp =/^\d{0,14}$/;
+ 
+ passportCheck.addEventListener("click", () => {
+   if (passportRegExp.test(passportInput.value)) {
+     passportResult.innerText = "Да";
+     passportResult.style.color = "green";
+   } else {
+     passportResult.innerText = "Почти ok";
+     passportResult.style.color = "red";
+   }
+ });
